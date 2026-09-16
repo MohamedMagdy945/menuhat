15cb8b8132e2117b45686cde9e42f0439619bf90
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-09-16 11:26:27 +0300

    feat: add main components pages

---
+++ src/app/features/offers/pages/offers/offers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-offers',
+  styleUrl: './offers.css',
+  templateUrl: './offers.html',
+})
+export class Offers {}
--- src/app/layouts/public-layout/components/navbar/mobile-navbar/mobile-navbar.ts
+++ src/app/layouts/public-layout/components/navbar/mobile-navbar/mobile-navbar.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   selector: 'app-mobile-navbar',
   styleUrl: './mobile-navbar.css',
   templateUrl: './mobile-navbar.html',
 })
-export class MobileNavbar {}
+export class MobileNavbar { }
