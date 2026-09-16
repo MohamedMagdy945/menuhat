87b5669f3fd2ca80902e9cadc158ac0b082d53df
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-09-16 12:44:56 +0300

    fea: add navbar desktop page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { PublicLayout } from './layouts/public-layout/public-layout';
 import { Home } from './pages/home/home';
+import { Cart } from './features/cart/pages/cart/cart';
+import { Favorites } from './features/favorites/pages/favorites/favorites';
+import { Offers } from './features/offers/pages/offers/offers';
+import { Profile } from './features/profile/pages/profile/profile';
 
 
 export const routes: Routes = [
@@ -12,6 +16,22 @@ export const routes: Routes = [
             {
                 path: '',
                 component: Home
+            },
+            {
+                path: 'cart',
+                component: Cart
+            },
+            {
+                path: 'favorites',
+                component: Favorites
+            },
+            {
+                path: 'offers',
+                component: Offers
+            },
+            {
+                path: 'profile',
+                component: Profile
             }
         ]
     }
