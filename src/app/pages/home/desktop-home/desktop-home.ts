e4b67dac2c517e3d0245b085a9e3e4761734a127
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-09-16 17:54:51 +0300

    feat: add first slide in home page

--- src/app/pages/home/desktop-home/desktop-home.ts
+++ src/app/pages/home/desktop-home/desktop-home.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   imports: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
   selector: 'app-desktop-home',
   styleUrl: './desktop-home.css',
   templateUrl: './desktop-home.html',
 })
-export class DesktopHome {}
+export class DesktopHome { }
