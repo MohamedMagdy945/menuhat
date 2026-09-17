0759d34f8cdb3e5e9e7eb68a460ef99349711ffd
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-09-17 12:12:55 +0300

    feat: add footer paegs

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MobileNavbar } from './components/navbar/mobile-navbar/mobile-navbar';
 import { DesktopNavbar } from './components/navbar/desktop-navbar/desktop-navbar';
+import { Footer } from '../../pages/footer/footer';
 
 @Component({
-  imports: [RouterOutlet, MobileNavbar, DesktopNavbar],
+  imports: [RouterOutlet, MobileNavbar, DesktopNavbar, Footer],
   selector: 'app-public-layout',
   styleUrl: './public-layout.css',
   templateUrl: './public-layout.html',
 })
-export class PublicLayout {}
+export class PublicLayout { }
