897a0363195d5bd51d2b4cc2d856545e526e4485
MohamedMagdy945 <mohamedmagdy9452@gmail.com>
2026-09-20 16:37:56 +0300

    feat: add  home page resturnat list static data

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,18 +1,135 @@
-import { Component, inject } from '@angular/core';
-import { DesktopHome } from './desktop-home/desktop-home';
-import { MobileHome } from './mobile-home/mobile-home';
-import { RestaurantList } from '../../features/restaurants/components/restaurant-desktop-list/restaurant-desktop-list';
+import { Component, inject, signal } from '@angular/core';
+
+import { DesktopHome } from './components/desktop-home/desktop-home';
+import { MobileHome } from './components/mobile-home/mobile-home';
+import { HomeRestaurantSection } from '../../features/restaurants/components/home-restaurant-section/home-restaurant-section';
+
 import { ResponsiveService } from '../../core/services/responsive.service';
-import { Restaurants } from '../../features/restaurants/pages/restaurants/restaurants';
+
+import { Restaurant } from '../../features/restaurants/models/restaurant';
+import { MOCK_RESTAURANT_PAGES } from '../../features/restaurants/mocks/restaurant.mock';
 
 @Component({
-  imports: [DesktopHome, MobileHome, Restaurants],
+  imports: [
+    DesktopHome,
+    MobileHome,
+    HomeRestaurantSection
+  ],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
 })
 export class Home {
+
   private readonly responsiveService = inject(ResponsiveService);
 
   readonly isMobile = this.responsiveService.isMobile;
+
+
+  // =========================
+  // Trending
+  // =========================
+
+  trendingRestaurants = signal<Restaurant[]>([
+    ...MOCK_RESTAURANT_PAGES[0]
+  ]);
+
+  private trendingPage = 0;
+
+
+  // =========================
+  // Popular
+  // =========================
+
+  popularRestaurants = signal<Restaurant[]>([
+    ...MOCK_RESTAURANT_PAGES[0]
+  ]);
+
+  private popularPage = 0;
+
+
+  // =========================
+  // Most Visited
+  // =========================
+
+  mostVisitedRestaurants = signal<Restaurant[]>([
+    ...MOCK_RESTAURANT_PAGES[0]
+  ]);
+
+  private mostVisitedPage = 0;
+
+
+  // =========================
+  // Trending
+  // =========================
+
+  loadMoreTrending(): void {
+
+    if (
+      this.trendingPage >=
+      MOCK_RESTAURANT_PAGES.length - 1
+    ) {
+      return;
+    }
+
+    this.trendingPage++;
+
+    const nextPage =
+      MOCK_RESTAURANT_PAGES[this.trendingPage];
+
+    this.trendingRestaurants.update(restaurants => [
+      ...restaurants,
+      ...nextPage
+    ]);
+  }
+
+
+  // =========================
+  // Popular
+  // =========================
+
+  loadMorePopular(): void {
+
+    if (
+      this.popularPage >=
+      MOCK_RESTAURANT_PAGES.length - 1
+    ) {
+      return;
+    }
+
+    this.popularPage++;
+
+    const nextPage =
+      MOCK_RESTAURANT_PAGES[this.popularPage];
+
+    this.popularRestaurants.update(restaurants => [
+      ...restaurants,
+      ...nextPage
+    ]);
+  }
+
+
+  // =========================
+  // Most Visited
+  // =========================
+
+  loadMoreMostVisited(): void {
+
+    if (
+      this.mostVisitedPage >=
+      MOCK_RESTAURANT_PAGES.length - 1
+    ) {
+      return;
+    }
+
+    this.mostVisitedPage++;
+
+    const nextPage =
+      MOCK_RESTAURANT_PAGES[this.mostVisitedPage];
+
+    this.mostVisitedRestaurants.update(restaurants => [
+      ...restaurants,
+      ...nextPage
+    ]);
+  }
 }
