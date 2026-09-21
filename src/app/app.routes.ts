915cd38653ce31ceb3c3c1e5fde241da2b603d81
Mohamed-Lotfi20 <mohamedlotfi3000@gmail.com>
2026-09-21 18:09:35 +0300

    feat: add verification page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Home } from './features/home/pages/home.component';
 import { Favorites } from './features/favorites/pages/favorites/favorites';
 import { Offers } from './features/offers/pages/offers/offers';
 import { Profile } from './features/profile/pages/profile/profile';
-import { Cart } from './features/cart/components/cart';
+import { Cart } from './features/cart/Components/cart';
+import { SendEmail } from './features/auth/pages/Register/components/SendEmail/send-email/send-email.component';
 
 
 export const routes: Routes = [
     { path: 'login', loadComponent: () => import('./features/auth/pages/login/components/login/login.component').then(m => m.Login) }, //canActivate: [logedGuard]
-
+    { path: 'sendEmail', loadComponent: () => import('./features/auth/pages/Register/components/SendEmail/send-email/send-email.component').then(m => m.SendEmail) },
+    { path: 'ValidateOtp', loadComponent: () => import('./features/auth/pages/Register/components/ValidateOtp/validate-otp/validate-otp.component').then(m => m.ValidateOtp) },
     {
         path: '', component: PublicLayout,
         children: [{ path: '', component: Home },
---
+++ src/app/features/auth/pages/Register/services/register.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Service } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../../core/environments/environment';
+
+@Service()
+export class RegisterService {
+  private readonly _HttpClient = inject(HttpClient);
+
+sendemail(data: object): Observable<any> {
+  return this._HttpClient.post(`${environment.apiUrl}/Auth/SendOtp`, data);
+}
+
+calidateOtp(data: object): Observable<any> {
+  return this._HttpClient.post(`${environment.apiUrl}/Auth/ValidateOtp`, data);
+}
+}
